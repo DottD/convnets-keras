965dd975209690680c256eeeec31a722a86f1a54
Juan Sebastian Navarro <jsnavarroa@unal.edu.co>
2017-08-16 11:23:55 -0500

    Update call to Convolution2D layer

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 from distutils.core import setup
 
 setup(name='convnetskeras',
-      version='0.1',
+      version='0.1.1',
       description='Pre-trained convnets in Keras',
       author='Leonard Blier',
       author_email='leonard.blier@ens.fr',
